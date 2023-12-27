74c59f1464a00e7cbf40f8fdfdccca0cd40fc5ce
Dave Augustus <davetweetlive@gmail.com>
2023-12-27 23:18:18 +0530

    Problem statement given
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

---
+++ call_time_calculator/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "time"
+
+type Call struct {
+	From      string
+	To        string
+	StartTime time.Time
+	EndTime   time.Time
+}
+
+func main() {
+	// Open the file
+
+	// Defer file.Close
+
+	// Read the CSV and parse into the []Call
+
+	// Check the name and calculate the actual time
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/saffrondigitsdesign_patterns_golang
 
 go 1.20
+
+require github.com/google/uuid v1.5.0 // indirect
